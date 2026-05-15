83695d3d82ec278ae678b0b6ef607a1752309132
IT25100651 <it25100651@my.sliit.lk>
2026-05-15 14:45:20 +0530

    Delete CustomCakeRepository.java

--- src/main/java/com/example/bakery/repository/CustomCakeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.bakery.repository;
-
-import com.example.bakery.model.CustomCake;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomCakeRepository extends JpaRepository<CustomCake, Long> {
-}
