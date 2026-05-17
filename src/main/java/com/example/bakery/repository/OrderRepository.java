976f695e068385d67992653522bd0941041e0ad6
it25101442 <it25101442@my.sliit.lk>
2026-05-17 15:53:39 +0530

    Update OrderRepository.java
    Added comment to clarify the purpose of the OrderRepository.

--- src/main/java/com/example/bakery/repository/OrderRepository.java
+++ src/main/java/com/example/bakery/repository/OrderRepository.java
@@ -1,7 +1,10 @@
 package com.example.bakery.repository;
 
 import com.example.bakery.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository; //Imports JpaRepository.
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
+
+
+//Use for access database
